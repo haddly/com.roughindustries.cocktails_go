473777de5fb87bd52c7922b4480b5bd22e3ca575
Rough Industries LLC <travishdc@roughindustries.com>
2016-12-17 14:01:40 -0600

    Update and rename helloworld.go to main.go

--- src/main/main.go
+++ src/main/main.go
@@ -4,8 +4,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-        "os"
-        "path/filepath"
+	"os"
+	"path/filepath"
 )
 
 type Page struct {
@@ -13,41 +13,42 @@ type Page struct {
 	Name  string
 }
 
+//render the page based on the name of the file provided
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-        
 	t, err := template.ParseFiles("./webcontent/" + tmpl + ".html")
-        if err != nil {
-            log.Fatal(err)
-        }
-        t.Execute(w, p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Execute(w, p)
 }
 
+//handle / requests to the server
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("indexHandler: " + r.URL.Path[1:])
+	//parse the url and get the information after the localhost:8080/
+	//stick that in the name
 	name := r.URL.Path[1:]
+	//load of the name and title for doing tempating
 	p := &Page{Title: "Commonwealth Cocktails", Name: name}
-	renderTemplate(w, "cocktailTemplate", p)
+	//apply the template page info to the index page
+	renderTemplate(w, "index", p)
 }
 
-func jamaicanQuaaludeHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("jamaicanQuaaludeHandler: " + r.URL.String())
-	p := &Page{Title: "Commonwealth Cocktails", Name: "Jamaican Quaalude"}
-	renderTemplate(w, "jamaicanQuaalude", p)
+func init(){
+	//Web Service and Web Page Handlers
+	http.HandleFunc("/", indexHandler)
 }
 
-func searchHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("searchHandler: " + r.URL.String())
-	p := &Page{Title: "Commonwealth Cocktails", Name: "Magarita"}
-	renderTemplate(w, "search", p)
-}
-
+//where it all starts
 func main() {
 	log.Println("Starting ... \n")
-        dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-        if err != nil {
-            log.Fatal(err)
-        }
-        log.Println(dir)
+	//print out the current directory
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(dir)
+
 	// Mandatory root-based resources and redirects for other resources
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./webcontent/images"))))
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./webcontent/css"))))
@@ -59,9 +60,8 @@ func main() {
 
 	//Web Service and Web Page Handlers
 	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/jamaicanQuaalude", jamaicanQuaaludeHandler)
-	http.HandleFunc("/search", searchHandler)
 
 	log.Println("Added Handlers ... Starting Server\n")
+	//this starts up the server
 	http.ListenAndServe(":8080", nil)
 }
