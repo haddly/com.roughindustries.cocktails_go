da810433dc1440f0d46b4cbb7cb88fbee328f93c
Rough <travishdc@roughindustries.com>
2017-01-05 20:15:17 +0000

    created basic product page.

--- src/controller/www/product.go
+++ src/controller/www/product.go
@@ -2,7 +2,6 @@
 package www
 
 import (
-	"html/template"
 	"log"
 	"model"
 	"net/http"
@@ -14,8 +13,20 @@ type Product struct {
 }
 
 //render the page based on the name of the file provided
-func (product *Product) RenderTemplate(w http.ResponseWriter, tmpl string, p *model.BaseProductWithBDG) {
-	t, err := template.ParseFiles("./view/webcontent/www/templates/"+tmpl+".html", "./view/webcontent/www/templates/head.html", "./view/webcontent/www/templates/ga.html", "./view/webcontent/www/templates/navbar.html", "./view/webcontent/www/templates/footer.html")
+func (product *Product) RenderBaseProductWithBDGTemplate(w http.ResponseWriter, tmpl string, p *model.BaseProductWithBDG) {
+	t, err := parseTempFiles(tmpl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = t.ExecuteTemplate(w, "base", p)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+//render the page based on the name of the file provided
+func (product *Product) RenderProductsTemplate(w http.ResponseWriter, tmpl string, p []model.Product) {
+	t, err := parseTempFiles(tmpl)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,34 +40,34 @@ func (product *Product) ProductHandler(w http.ResponseWriter, r *http.Request) {
 	var p *model.BaseProductWithBDG
 	u, err := url.Parse(r.URL.String())
 	if err != nil {
-		product.RenderTemplate(w, "404", p)
+		product.RenderBaseProductWithBDGTemplate(w, "404", p)
 	}
 	//log.Println("Product: " + r.URL.Path[1:])
 	m, err := url.ParseQuery(u.RawQuery)
 	if err != nil {
-		product.RenderTemplate(w, "404", p)
+		product.RenderBaseProductWithBDGTemplate(w, "404", p)
 	}
 	if len(m["ID"]) == 0 {
-		product.RenderTemplate(w, "404", p)
+		product.RenderBaseProductWithBDGTemplate(w, "404", p)
 	} else {
 		//log.Println("ID: " + m["ID"][0])
 
 		//apply the template page info to the index page
 		id, _ := strconv.Atoi(m["ID"][0])
 		if len(model.Products) <= id-1 {
-			product.RenderTemplate(w, "404", p)
+			product.RenderBaseProductWithBDGTemplate(w, "404", p)
 		} else {
 			p = model.GetBaseProductByIDWithBDG(id)
-			product.RenderTemplate(w, "product", p)
+			product.RenderBaseProductWithBDGTemplate(w, "product", p)
 		}
 	}
 }
 
 func (product *Product) ProductsHandler(w http.ResponseWriter, r *http.Request) {
-	var p *model.BaseProductWithBDG
+	var p []model.Product
 
-	p = model.GetBaseProductWithBDG()
-	product.RenderTemplate(w, "product", p)
+	p = model.GetProducts()
+	product.RenderProductsTemplate(w, "products", p)
 }
 
 func (product *Product) Init() {
